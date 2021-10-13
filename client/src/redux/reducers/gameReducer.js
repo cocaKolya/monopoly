fa8b340801e0d86d9904798f36e04ff6f24d3a1f
Nikolay <nelidovn@gmail.com>
2021-10-13 20:39:15 +0300

    add create socket

--- client/src/redux/reducers/gameReducer.js
+++ client/src/redux/reducers/gameReducer.js
@@ -1,5 +1,5 @@
-import { START_GAME_SOCKET } from '../../constants/socket';
-import { ADD_GAME, ADD_USER_TO_GAME, GET_GAMES } from '../types/gameTypes';
+import { CREATE_GAME_SOCKET, START_GAME_SOCKET } from '../../constants/socket';
+import { ADD_USER_TO_GAME, GET_GAMES } from '../types/gameTypes';
 
 export const gameReducer = (state = [], action) => {
   const { type, payload } = action;
@@ -11,10 +11,12 @@ export const gameReducer = (state = [], action) => {
       return state;
     }
     case START_GAME_SOCKET: {
-
       return state.map((el) =>
         el.id === payload ? { ...el, inprocess: true } : el
       );
+    }
+    case CREATE_GAME_SOCKET: {
+      return [...state, payload];
     }
 
     default:
