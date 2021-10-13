27de8465da3dba8a5f9fb18271ab1a5af4f8612a
Oleg <vysockiy91@gmail.com>
2021-10-13 17:49:03 +0300

    create jsx LogIn

---
+++ client/src/components/LogIn.jsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { checkUser } from '../redux/actions/userAction';
+
+const RegFormForma = styled('form')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 1000px;
+`;
+
+function LogIn() {
+  const [formInput, setForm] = useState({});
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/home');
+    }
+  }, [user]);
+
+  const inputChange = (e) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    console.log(formInput);
+  };
+  const submitForm = (e) => {
+    e.preventDefault();
+    dispatch(checkUser(formInput));
+  };
+
+  return (
+    <>
+      <RegFormForma onSubmit={(e) => submitForm(e)}>
+        email
+        <input name='email' value={formInput.email} onChange={inputChange} />
+        pass
+        <input
+          name='password'
+          value={formInput.password}
+          onChange={inputChange}
+        />
+        <button type='submit'>reg</button>
+      </RegFormForma>
+    </>
+  );
+}
+export default LogIn;
